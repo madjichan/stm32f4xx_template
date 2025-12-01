01225efeaa0fdef93df0f50a2dfb311c4fa3ecbf
madjichan <madjichan@gmail.com>
2025-12-01 20:36:35 +0300

    adopt test code for black pill

--- src/main.c
+++ src/main.c
@@ -2,11 +2,11 @@
 
 
 int main(void) {
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-    GPIOA->MODER |= (1U << (5 * 2));
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+    GPIOC->MODER |= (1U << (13 * 2));
 
     while (1) {
-        GPIOA->ODR ^= (1U << 5);
+        GPIOC->ODR ^= (1U << 13);
         for (volatile int i = 0; i < 500000; i++);
     }
 }
